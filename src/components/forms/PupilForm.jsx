6692ea2645aa8272012bdd8f0dacf72bdd5399b6
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-07-15 19:12:01 +0300

    Fix some issues

--- src/components/forms/PupilForm.jsx
+++ src/components/forms/PupilForm.jsx
@@ -10,7 +10,7 @@ import searchService from '../../services/search'
 import moment from 'moment'
 import { paymentObligations,
 	personalDataProcessing, benefitsExplained } from '../../data/formTexts.json'
-import { findByPropertyValue, phoneNumber as phonePattern,
+import { phoneNumber as phonePattern,
 	formatPhoneNumber, trimObject } from '../../utils'
 
 import { Formik } from 'formik'
@@ -33,8 +33,6 @@ const PupilForm = ({
 	fetchingData,
 	mode,
 	closeModal }) => {
-
-	// console.log('Super user', user.superUser, 'mode', mode)
 
 	const history = useHistory()
 	const unmounted = useRef(false)
@@ -87,25 +85,25 @@ const PupilForm = ({
 
 	// handle edit or create
 	const handlePupil = async (values, setErrors, resetForm) => {
-		// replace human readable specialty with id
-		const { id } = findByPropertyValue(values.specialty, 'title', specialtiesData) //??
+
+		// replace human readable specialty title with id
+		const { id: specialty } = specialtiesData.find(specialty => specialty.title === values.specialty)
 		values = { ...values,
-			specialty: id
+			specialty
 		}
+
 		// all this looks ugly
-		const assignedToUser = await searchService.users({ value: values.assignedTo })
-		if (assignedToUser[0]) {
-			values = { ...values,
-				assignedTo: assignedToUser[0].id
-			}
-		} else {
-			values = { ...values,
-				assignedTo: user.email
-			}
+		if (values.assignedTo) {
+			const users = await searchService.users({ value: values.assignedTo })
+			const { id: assignedTo } = users.find(user => user.email === values.assignedTo)
+			if (assignedTo) values = { ...values, assignedTo }
 		}
+
 		if (editMode) values = { ...values,
 			schoolClasses: values.schoolClasses.map(item => item.id),
 		}
+
+		console.log('Sending this', values)
 
 		setProcessingForm(true)
 		editMode
@@ -117,7 +115,10 @@ const PupilForm = ({
 
 	const publicApply = (values, setErrors, resetForm) => {
 		// this one is a little different
-		pupilsService.publicApply(values)
+		// remove assignedTo, as it is a public form
+		// eslint-disable-next-line
+		const { assignedTo, ...valuesToSend } = values
+		pupilsService.publicApply(valuesToSend)
 			.then(() => {
 				setNotification({
 					message: 'Ваша заява була успішно додана.',
@@ -144,7 +145,6 @@ const PupilForm = ({
 
 	const addPupil = (values, setErrors, resetForm) => {
 		const valuesToSend = { ...values, assignedTo: user.id }
-		console.log(valuesToSend)
 		createPupil(valuesToSend)
 			.then(() => {
 				setNotification({
@@ -172,7 +172,8 @@ const PupilForm = ({
 
 	const editPupil = (values, setErrors) => {
 		// remove teachers and schoolClasses
-		// as they are not updated through this form
+		// as they are not updated through this
+		// instance of the form
 		// eslint-disable-next-line
 		const { teachers, schoolClasses, ...valuesToSend } = values
 		updatePupil(pupil.id, valuesToSend)
@@ -201,7 +202,6 @@ const PupilForm = ({
 	}
 
 	const getUsers = value => {
-		console.log(value)
 		if (value.length >= 2) {
 			setFetchingData(true)
 			const query = { value }
