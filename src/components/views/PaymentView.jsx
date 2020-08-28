16b2e8b96e612ddb374cb9edff455d51bc2271ff
Gavrilenko <gavrilenko.georgi@gmail.com>
2020-08-28 15:41:01 +0300

    Update payment form

--- src/components/views/PaymentView.jsx
+++ src/components/views/PaymentView.jsx
@@ -12,22 +12,29 @@ const PaymentView = ({ match }) => {
 		<Container className="text-center">
 			<Row className="pt-4 d-flex justify-content-center">
 				<Col lg={7} className="pt-4">
-					<h6 className="text-warning border border-warning rounded py-2 mb-4">
-						<em>тестовий режим</em>
-					</h6>
-					{status === 'success'
+					{status
 						? <>
-							<FontAwesomeIcon icon={faReceipt} className="fa-lg payment-status-icon"/>
-							<h3 className="text-success pt-4">
-								Дякуємо, ваш платіж отримано
-							</h3>
+							{status === 'form'
+								? <><h3 className="custom-font pb-4">
+									Оплата навчання
+								</h3>
+								<PaymentForm />
+								</>
+								: <>
+									{status === 'success'
+										? <><FontAwesomeIcon icon={faReceipt} className="fa-lg payment-status-icon"/>
+											<h3 className="text-success pt-4">
+												Дякуємо, ваш платіж отримано
+											</h3></>
+										: <><h3 className="pt-4">
+											Статус вашого платіжу {status}
+										</h3>
+										</>
+									}
+								</>
+							}
 						</>
-						: <>
-							<h3 className="custom-font pb-4">
-								Оплата навчання
-							</h3>
-							<PaymentForm />
-						</>
+						: null
 					}
 				</Col>
 			</Row>
