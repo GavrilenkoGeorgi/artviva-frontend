c322466d979d2185ece73d7effe3968da333910b
GavrilenkoGeorgi <gavrilenko.georgi@gmail.com>
2022-01-10 15:39:26 +0200

    Add "Legal page"

---
+++ src/components/views/legal/LegalDocs.jsx
@@ -0,0 +1,149 @@
+import React from 'react'
+
+import CommonLayout from '../../CommonLayout'
+import { Container } from 'react-bootstrap'
+
+const LegalDocs = () => {
+
+	return <CommonLayout>
+		<Container className="page-container border mt-5">
+			<h1 className="custom-font mt-5">
+				Нормативна база, якою керується у своїй діяльності школа
+			</h1>
+
+			<h2>Правові документи:</h2>
+			<ul>
+				<li>
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/254%D0%BA/96-%D0%B2%D1%80#Text">
+						Конституція України
+					</a><br />
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/254%D0%BA/96-%D0%B2%D1%80#Text">
+						https://zakon.rada.gov.ua/laws/show/254%D0%BA/96-%D0%B2%D1%80
+					</a>
+				</li>
+					<li>
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/995_021#Text">
+						Конвенція про права дитини
+					</a><br />
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/995_021#Text">
+						https://zakon.rada.gov.ua/laws/show/995_021
+					</a>
+					</li>
+			</ul>
+
+			<h2>Закони України:</h2>
+			<ul>
+				<li>
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/1841-14#Text">
+						Закон України «Про позашкільну освіту»
+					</a><br />
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/1841-14#Text">
+						https://zakon.rada.gov.ua/laws/show/1841-14
+					</a>
+				</li>
+
+				<li>
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/2939-17#Text">
+						Закон України «Про доступ до публічної інформації»
+					</a><br />
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/2939-17#Text">
+						https://zakon.rada.gov.ua/laws/show/2939-17
+					</a>
+				</li>
+
+				<li>
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/2694-12#Text">
+						Закон України «Про охорону праці»
+					</a><br />
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/2694-12#Text">
+						https://zakon.rada.gov.ua/laws/show/2694-12
+					</a>
+				</li>
+
+				<li>
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/2402-14#Text">
+						Закон України «Про охорону дитинства»
+					</a><br />
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/2402-14#Text">
+						https://zakon.rada.gov.ua/laws/show/2694-12
+					</a>
+				</li>
+
+				<li>
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/322-08#Text">
+						Кодекс законів про працю України
+					</a><br />
+					<a target="_blank" rel="noopener noreferrer" href="https://zakon.rada.gov.ua/laws/show/322-08#Text">
+					https://zakon.rada.gov.ua/laws/show/322-08
+					</a>
+				</li>
+			</ul>
+
+			<h2>Порядок реагування на доведені випадки булінгу</h2>
+			<p>
+				Порядок реагування на доведені випадки булінгу (цькування)
+				в закладі освіти (відповідно до листа МОНУ від 29.01.2019
+				№1/11-881 "Рекомендації для закладів освіти щодо застосування
+				норм Закону України "Про внесення змін до деяких законодавчих
+				актів України щодо протидії булінгу (цькування) від 18.12.2018 №2657-VIII")
+			</p>
+			<p>
+				В разі підтвердження факту вчинення булінгу (цькування), за результатами
+				розслідування та висновків Комісії, створеної у закладі освіти з розгляду
+				випадків булінгу, повідомляються уповноважені підрозділи органів
+				Національної поліції України та служби у справах дітей про випадки
+				булінгу (цькування) в закладі освіти.
+			</p>
+			<p>
+				Виконується рішення та рекомендації комісії з розгляду випадків булінгу
+				(цькування) в закладі освіти.
+			</p>
+			<p>
+				Надаються соціальні та психолого-педагогічні послуги здобувачам освіти,
+				які вчинили булінг, стали його свідками або постраждали від булінгу.
+			</p>
+			<p>
+				Визначаються відповідальні особи, причетні до булінгу (цькування) та
+				накладаються адміністративні стягнення:
+			</p>
+			<ul>
+				<li>
+					цькування неповнолітнього карається штрафом від 50 до 100
+					неоподатковуваних мінімумів доходів громадян (850 та 1700 гривень
+					відповідно) або громадськими роботами від 20 до 40 годин.
+				</li>
+				<li>
+					така ж поведінка, вчинена групою осіб або повторно протягом року
+					після накладення адміністративного стягнення, передбачає штраф від
+					1700 гривень до 3400 гривень або громадськими роботами від 40 до 60 годин.
+				</li>
+				<li>
+					за булінг, вчинений малолітніми або неповнолітніми особами віком від
+					14 до 16 років,тягне за собою накладання штрафу на батьків або осіб,
+					які їх замінюють.
+				</li>
+			</ul>
+
+			<h2>Інформаційна відкритість</h2>
+				<ul>
+					<li>
+						<a href="/pdf/statut-artviva-2020.pdf">
+							Статут школи мистецтв «АРТ ВІВА»
+						</a><br />
+						<a href="/pdf/statut-artviva-2020.pdf">
+							завантажити PDF
+						</a>
+					</li>
+				</ul>
+
+			<h3>Виписка з державного реєстру</h3>
+			<p className="text-center">
+				<img src="/img/legal/excerpt_page_1.jpg" alt="Виписка з державного реєстру сторінка 1." />
+				<img src="/img/legal/excerpt_page_2.jpg" alt="Виписка з державного реєстру сторінка 2." />
+			</p>
+
+		</Container>
+	</CommonLayout>
+}
+
+export default LegalDocs
