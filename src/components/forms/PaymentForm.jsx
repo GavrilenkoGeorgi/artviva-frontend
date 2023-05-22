1ba4a4a6cabd7ee21f02fa0273b0c03042d1824d
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-05-22 13:35:46 +0300

    Update payment form: info modal
    - Info modal with info on how benefits are applied
    - Chores: CSS cleanup

--- src/components/forms/PaymentForm.jsx
+++ src/components/forms/PaymentForm.jsx
@@ -15,10 +15,14 @@ import { v4 as uuidv4 } from 'uuid'
 
 import { Col, Form } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHryvnia } from '@fortawesome/free-solid-svg-icons'
+import { faHryvnia, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { BtnWithSpinner, Button } from '../common/buttons'
 import { SimpleSpinner } from '../common/spinners'
 import { TextInput } from './components'
+import { InfoModal } from '../common/modals'
+
+import styles from './PaymentForm.module.sass'
+import modalsTextContent from '../../data/modalsTextContent.json'
 
 const PaymentForm = ({
 	reCaptchaScore,
@@ -201,6 +205,18 @@ const PaymentForm = ({
 		}
 	}, [reCaptchaScore, paymentData, handlePayment, setNotification, setProcessingForm])
 
+	// Benefits info modal
+	const [infoModalVis, setInfoModalVis] = useState(false)
+	const [infoModalText, setInfoModalText] = useState({})
+	const [infoModalTitle, setInfoModalTitle] = useState('')
+
+	const openInfoModal = () => {
+		const modalText = modalsTextContent.paymentBenefitsInfo
+		setInfoModalTitle(modalText.title)
+		setInfoModalText(modalText.text)
+		setInfoModalVis(true)
+	}
+
 	// Form schema
 	const paymentFormSchema = Yup.object().shape({
 		teacher: Yup.string().trim()
@@ -222,253 +238,270 @@ const PaymentForm = ({
 
 	// Form itself
 	return (
-		<Formik
-			initialValues={{
-				teacher: '',
-				pupil: '',
-				specialty: '',
-				months: [],
-				benefits: 0
-			}}
-			onSubmit={async (values) => {
-				setProcessingForm(true)
-				setPaymentData(values)
-				getRecaptchaScore()
-			}}
-			onReset={() => {
-				setOrderData({ ...orderData, months: [], cost: null, benefits: 0 })
-				setTotal(null)
-			}}
-			validationSchema={paymentFormSchema}
-		>
-			{({ handleSubmit,
-				handleChange,
-				handleBlur,
-				handleReset,
-				values,
-				touched,
-				errors,
-			}) => (
-				<Form
-					ref={paymentFormEl}
-					data-cy="payment-form"
-					noValidate
-					onSubmit={handleSubmit}
-					onChange={event => processOrderData(event)}
-					method="POST"
-					action={process.env.REACT_APP_LIQPAY_API_URL}
-					acceptCharset="utf-8"
-					className="text-left"
-				>
+		<>
+			<Formik
+				initialValues={{
+					teacher: '',
+					pupil: '',
+					specialty: '',
+					months: [],
+					benefits: 0
+				}}
+				onSubmit={async (values) => {
+					setProcessingForm(true)
+					setPaymentData(values)
+					getRecaptchaScore()
+				}}
+				onReset={() => {
+					setOrderData({ ...orderData, months: [], cost: null, benefits: 0 })
+					setTotal(null)
+				}}
+				validationSchema={paymentFormSchema}
+			>
+				{({ handleSubmit,
+					handleChange,
+					handleBlur,
+					handleReset,
+					values,
+					touched,
+					errors,
+				}) => (
+					<Form
+						ref={paymentFormEl}
+						data-cy="payment-form"
+						noValidate
+						onSubmit={handleSubmit}
+						onChange={event => processOrderData(event)}
+						method="POST"
+						action={process.env.REACT_APP_LIQPAY_API_URL}
+						acceptCharset="utf-8"
+						className="text-left"
+					>
 
-					{/* Teacher's name input */}
-					<Form.Row>
-						<Form.Group
-							controlId="teacher-name-input"
-							as={Col}
-						>
-							<Form.Label>Викладач<span className="form-required-mark"> *</span>
-								{searching
-									? <SimpleSpinner
-										className="ml-1"
-										size="sm"
-										variant="success"
-									/>
-									: null
-								}
-							</Form.Label>
-							<Form.Control
-								type="text"
-								name="teacher"
-								list="teachers-list"
-								autoComplete="off"
-								data-cy="teacher-name-input"
-								onChange={handleChange}
-								onKeyUp={event => getTeachers(event.target.value.trim())}
-								onBlur={handleBlur}
-								value={values.teacher}
-								isValid={touched.teacher && !errors.teacher}
-								isInvalid={touched.teacher && !!errors.teacher}
-							/>
-							<datalist id="teachers-list">
-								{teachersList.map((name) =>
-									<option key={name} value={name} />
-								)}
-							</datalist>
-							<Form.Control.Feedback type="invalid">
-								{errors.teacher}
-							</Form.Control.Feedback>
-						</Form.Group>
-					</Form.Row>
-
-					<TextInput
-						label="Прізвище учня"
-						name="pupil"
-						dataCy="pupil-name-input"
-						onChange={handleChange}
-						value={values.pupil}
-						touched={touched.pupil}
-						errors={errors.pupil}
-					/>
-
-					{/* Specialty input */}
-					<Form.Row>
-						<Form.Group
-							controlId="specialty-input"
-							as={Col}
-						>
-							<Form.Label>
-								Предмет
-							</Form.Label>
-							<Form.Control
-								as="select"
-								custom
-								name="specialty"
-								data-cy="specialty-input"
-								onChange={handleChange}
-								onBlur={handleBlur}
-								value={values.specialty}
-								isValid={touched.specialty && !errors.specialty}
-								isInvalid={touched.specialty && !!errors.specialty}
+						{/* Teacher's name input */}
+						<Form.Row>
+							<Form.Group
+								controlId="teacher-name-input"
+								as={Col}
 							>
-								<option>Виберіть...</option>
-								{specialtiesNames.map(specialty =>
-									<option value={specialty} key={specialty}>{specialty}</option>
-								)}
-							</Form.Control>
-							<Form.Control.Feedback type="invalid">
-								{errors.specialty}
-							</Form.Control.Feedback>
-						</Form.Group>
-					</Form.Row>
+								<Form.Label>Викладач<span className="form-required-mark"> *</span>
+									{searching
+										? <SimpleSpinner
+											className="ml-1"
+											size="sm"
+											variant="success"
+										/>
+										: null
+									}
+								</Form.Label>
+								<Form.Control
+									type="text"
+									name="teacher"
+									list="teachers-list"
+									autoComplete="off"
+									data-cy="teacher-name-input"
+									onChange={handleChange}
+									onKeyUp={event => getTeachers(event.target.value.trim())}
+									onBlur={handleBlur}
+									value={values.teacher}
+									isValid={touched.teacher && !errors.teacher}
+									isInvalid={touched.teacher && !!errors.teacher}
+								/>
+								<datalist id="teachers-list">
+									{teachersList.map((name) =>
+										<option key={name} value={name} />
+									)}
+								</datalist>
+								<Form.Control.Feedback type="invalid">
+									{errors.teacher}
+								</Form.Control.Feedback>
+							</Form.Group>
+						</Form.Row>
 
-					<Form.Row>
-						<Form.Group
-							controlId="benefits-input"
-							as={Col}
-						>
-							<Form.Label>
-								Пільги?
-							</Form.Label>
-							<Form.Control
-								as="select"
-								custom
-								name="benefits"
-								data-cy="benefits-input"
-								onChange={handleChange}
-								onBlur={handleBlur}
-								value={values.benefits}
-								isValid={touched.benefits && !errors.benefits}
-								isInvalid={touched.benefits && !!errors.benefits}
-							>
-								<option value={0}>Виберіть...</option>
-								<option value={50}>Моя дитина має пільги у розмірі 50%</option>
-							</Form.Control>
-							<Form.Control.Feedback type="invalid">
-								{errors.benefits}
-							</Form.Control.Feedback>
-						</Form.Group>
-
-					</Form.Row>
-
-					<Form.Row>
-						<FieldArray
-							name='months'
+						<TextInput
+							label="Прізвище учня"
+							name="pupil"
+							dataCy="pupil-name-input"
 							onChange={handleChange}
-							onBlur={handleBlur}
-							render={arrayHelpers => (
-								<Col className="d-flex flex-wrap pt-3">
-									{months.map(month => (
-										<Col key={month} xs={4} className="my-2 px-1">
-											<Form.Check
-												inline
-												custom
-												name="months"
-												type="checkbox"
-												id={month}
-												value={month}
-												label={month}
-												checked={values.months.includes(month)}
-												isValid={touched.months && !errors.months}
-												isInvalid={touched.months && !!errors.months}
-												onChange={event => {
-													if (event.target.checked) arrayHelpers.push(month)
-													else {
-														const index = values.months.indexOf(month)
-														arrayHelpers.remove(index)
-													}
-												}}
-											/>
-										</Col>
-									))}
-									<ErrorMessage
-										name="months"
-										render={msg => (
-											<span className="form-validation-error">
-												{msg}
-											</span>
-										)}
-									/>
-								</Col>
-							)}
+							value={values.pupil}
+							touched={touched.pupil}
+							errors={errors.pupil}
 						/>
-					</Form.Row>
 
-					<Form.Row className="py-3 d-flex justify-content-center">
-						{total
-							? <>
-								<Col xs={11} className="text-right payment-total-message">
-									Всього: <span className="total-amount">
-										{showTotals(calculatePercent(process.env.REACT_APP_LIQPAY_API_PERCENT, total)
-											.toFixed(2), total)}
+						{/* Specialty input */}
+						<Form.Row>
+							<Form.Group
+								controlId="specialty-input"
+								as={Col}
+							>
+								<Form.Label>
+									Предмет
+								</Form.Label>
+								<Form.Control
+									as="select"
+									custom
+									name="specialty"
+									data-cy="specialty-input"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.specialty}
+									isValid={touched.specialty && !errors.specialty}
+									isInvalid={touched.specialty && !!errors.specialty}
+								>
+									<option>Виберіть...</option>
+									{specialtiesNames.map(specialty =>
+										<option value={specialty} key={specialty}>{specialty}</option>
+									)}
+								</Form.Control>
+								<Form.Control.Feedback type="invalid">
+									{errors.specialty}
+								</Form.Control.Feedback>
+							</Form.Group>
+						</Form.Row>
+
+						<Form.Row>
+							<Form.Group
+								controlId="benefits-input"
+								as={Col}
+							>
+								<Form.Label className={styles.wideLabel}>
+									<span>
+										Пільги?
 									</span>
-								</Col>
-								<Col xs={1} className="d-flex align-items-center">
-									<FontAwesomeIcon icon={faHryvnia} />
-								</Col>
-							</>
-							: <p className="payment-total-message">
-								<em>Заповніть форму для розрахунку вартості</em>
-							</p>
-						}
-						<Col xs={12} className="text-right">
-							<em className="text-muted small">
-								В тому числі відсоток 1.75% за зручність, покращений сервіс.
-							</em>
-						</Col>
-					</Form.Row>
+									<FontAwesomeIcon
+										icon={faInfoCircle}
+										className={styles.infoIcon}
+										onClick={() => openInfoModal('benefits')}
+									/>
+								</Form.Label>
+								<Form.Control
+									as="select"
+									custom
+									name="benefits"
+									data-cy="benefits-input"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.benefits}
+									isValid={touched.benefits && !errors.benefits}
+									isInvalid={touched.benefits && !!errors.benefits}
+								>
+									<option value={0}>Виберіть...</option>
+									<option value={50}>Моя дитина має пільги у розмірі 50%</option>
+								</Form.Control>
+								<Form.Control.Feedback type="invalid">
+									{errors.benefits}
+								</Form.Control.Feedback>
+							</Form.Group>
 
-					<input type="hidden" name="data" value={liqpayData.data || ''} />
-					<input type="hidden" name="signature" value={liqpayData.signature || ''} />
-					<input type="hidden" name="language" value="uk" />
+						</Form.Row>
 
-					{/* Pay button */}
-					<Form.Row className="d-flex justify-content-around py-4">
-						<Col>
-							<BtnWithSpinner
-								block
-								type="submit"
-								variant="primary"
-								dataCy="pay-button"
-								loadingState={processingForm}
-								className="primary-color-shadow"
-								label="Оплатити"
+						<Form.Row>
+							<FieldArray
+								name='months'
+								onChange={handleChange}
+								onBlur={handleBlur}
+								render={arrayHelpers => (
+									<Col className="d-flex flex-wrap pt-3">
+										{months.map(month => (
+											<Col key={month} xs={4} className="my-2 px-1">
+												<Form.Check
+													inline
+													custom
+													name="months"
+													type="checkbox"
+													id={month}
+													value={month}
+													label={month}
+													checked={values.months.includes(month)}
+													isValid={touched.months && !errors.months}
+													isInvalid={touched.months && !!errors.months}
+													onChange={event => {
+														if (event.target.checked) arrayHelpers.push(month)
+														else {
+															const index = values.months.indexOf(month)
+															arrayHelpers.remove(index)
+														}
+													}}
+												/>
+											</Col>
+										))}
+										<ErrorMessage
+											name="months"
+											render={msg => (
+												<span className="form-validation-error">
+													{msg}
+												</span>
+											)}
+										/>
+									</Col>
+								)}
 							/>
-						</Col>
-						<Col>
-							<Button
-								block
-								variant="light"
-								dataCy="reset-pay-form-btn"
-								onClick={handleReset}
-								label="Очистити"
-							/>
-						</Col>
-					</Form.Row>
+						</Form.Row>
 
-				</Form>
-			)}
-		</Formik>
+						<Form.Row className="py-3 d-flex justify-content-center">
+							{total
+								? <>
+									<Col xs={11} className={styles.paymentTotalMessage}>
+										Всього: <span className={styles.totalAmount}>
+											{showTotals(calculatePercent(process.env.REACT_APP_LIQPAY_API_PERCENT, total)
+												.toFixed(2), total)}
+										</span>
+									</Col>
+									<Col xs={1} className="d-flex align-items-center">
+										<FontAwesomeIcon icon={faHryvnia} />
+									</Col>
+								</>
+								: <p className={styles.paymentTotalMessage}>
+									<em>Заповніть форму для розрахунку вартості</em>
+								</p>
+							}
+							<Col xs={12} className="text-right">
+								<em className="text-muted small">
+									В тому числі відсоток 1.75% за зручність, покращений сервіс.
+								</em>
+							</Col>
+						</Form.Row>
+
+						<input type="hidden" name="data" value={liqpayData.data || ''} />
+						<input type="hidden" name="signature" value={liqpayData.signature || ''} />
+						<input type="hidden" name="language" value="uk" />
+
+						{/* Pay button */}
+						<Form.Row className="d-flex justify-content-around py-4">
+							<Col>
+								<BtnWithSpinner
+									block
+									type="submit"
+									variant="primary"
+									dataCy="pay-button"
+									loadingState={processingForm}
+									className="primary-color-shadow"
+									label="Оплатити"
+								/>
+							</Col>
+							<Col>
+								<Button
+									block
+									variant="light"
+									dataCy="reset-pay-form-btn"
+									onClick={handleReset}
+									label="Очистити"
+								/>
+							</Col>
+						</Form.Row>
+
+					</Form>
+				)}
+			</Formik>
+			<InfoModal
+				title={infoModalTitle}
+				text={infoModalText}
+				centered
+				show={infoModalVis}
+				btnLabel={'Зрозуміло'}
+				onHide={() => setInfoModalVis(!infoModalVis)}
+			/>
+		</>
 	)
 }
 
