1940ed9c42da92ba9d26a5a8e5d43ff2330561b2
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-06-09 13:38:30 +0300

    Update 'Contacts' page, new CTA
    - New CTA that redirects to the main page contact form
    - Updated layout for the map
    - Some cleanup

--- src/components/contactMessage/SendContactMessage.jsx
+++ src/components/contactMessage/SendContactMessage.jsx
@@ -77,7 +77,7 @@ const SendContactMessage = ({
 		}
 	}, [reCaptchaScore, messageData, sendContactMessage])
 
-	return <Container className="py-4 my-4 py-sm-5 my-sm-5 fluid1">
+	return <Container id="contact-form-anchor" className="py-4 my-4 py-sm-5 my-sm-5">
 		<Row className="justify-content-center">
 			<h2 className="text-center custom-font">
 				Зворотній зв&apos;язок
--- src/components/views/public/ContactsView.jsx
+++ src/components/views/public/ContactsView.jsx
@@ -4,14 +4,15 @@ import ContactMap from '../../common/ContactMap'
 import departmentsData from '../../../data/departments.json'
 import { Container, Row } from 'react-bootstrap'
 import DepartmentCard from '../../common/DepartmentCard'
+import ButtonLink from '../../common/buttons/ButtonLink'
+
+import styles from './ContactsView.module.sass'
 
 const ContactsView = () => {
 
 	const { departments } = departmentsData
 	const mapStyles = {
-		height: '70vh',
-		width: '100%',
-		top: '4rem'
+		height: '35rem'
 	}
 
 	const initialCenter = {
@@ -19,33 +20,55 @@ const ContactsView = () => {
 		lng: 30.143868
 	}
 
-	const largePaddingTop = {
-		// that map of google's is somehow absolutely positioned
-		paddingTop: '75vh'
-	}
-
 	return <>
 		<Helmet>
 			<title>Контакти школи мистецтв «АРТ ВІВА»</title>
-			<meta name="description" content="Карта, яка показує всі наші відділення та їх контакти." />
+			<meta
+				name="description"
+				content="Карта, яка показує всі наші відділення та їх контакти."
+			/>
 		</Helmet>
-		<ContactMap
-			mapStyles={mapStyles}
-			initialCenter={initialCenter}
-			zoom={11}
-			departments={departments}
-		/>
-		<h1 className="text-center py-2 custom-font">Філії</h1>
-		<Container style={largePaddingTop}>
-			<Row className="d-flex justify-content-around px-2">
-				{departments.map(department =>
-					<DepartmentCard
-						key={department.id}
-						department={department}
+		<section className={styles.mapSection}>
+			<h1 className='custom-font'>
+				Філії
+			</h1>
+			<div className={styles.mapWrapper}>
+				<div className={styles.mapContainer}>
+					<ContactMap
+						mapStyles={mapStyles}
+						initialCenter={initialCenter}
+						zoom={12}
+						departments={departments}
 					/>
-				)}
-			</Row>
-		</Container>
+				</div>
+			</div>
+			<div className={styles.ctaContainer}>
+				<p className={`${styles.ctaLead} custom-font`}>
+					Розкрийте свій музичний талант!
+				</p>
+				<p className={styles.ctaText}>
+					Приєднуйтесь до нас та відкрийте безмежний світ музичного мистецтва.
+					В нашій школі ми допоможемо вам освоїти класику та відкрити нові композиції.
+					Запишіться вже зараз і дайте вашим мелодіям звучати!
+				</p>
+				<ButtonLink
+					variant="primary"
+					route="/#contact-form-anchor"
+					label="Запишіться зараз"
+					className="primary-color-shadow max-width-btn"
+				/>
+			</div>
+			<Container className={styles.depCardsContainer}>
+				<Row>
+					{departments.map(department =>
+						<DepartmentCard
+							key={department.id}
+							department={department}
+						/>
+					)}
+				</Row>
+			</Container>
+		</section>
 	</>
 }
 
