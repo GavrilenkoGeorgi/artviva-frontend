f18f4369c12f52fbc30eba86c61dab7933db84c3
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-07-05 20:02:11 +0300

    Add paginated list of post to the news page
    - Facebook Graph API is used to get posts feed from the page

--- src/components/views/public/BlogView.jsx
+++ src/components/views/public/BlogView.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
-import { FacebookProvider, Page } from 'react-facebook'
 import { Container } from 'react-bootstrap'
+import axios from 'axios'
+
+import styles from './BlogView.module.sass'
 
 const BlogView = () => {
-	const windowWidth = window.innerWidth
+
+	const [ pageAccessToken, setPageAccessToken ] = useState(null)
+	const [ facebookPosts, setFacebookPosts ] = useState([])
+
+	useEffect(() => {
+		const pageAccess = `https://graph.facebook.com/${process.env.REACT_APP_FACEBOOK_PAGE_ID}?fields=access_token&access_token=${process.env.REACT_APP_FACEBOOK_ACCESS_TOKEN}`
+		axios.get(pageAccess).then((response) => {
+			setPageAccessToken(response.data.access_token)
+		})
+	}, [])
+
+	useEffect(() => {
+		if (pageAccessToken) {
+			const feedURL = `https://graph.facebook.com/v2.2/${process.env.REACT_APP_FACEBOOK_PAGE_ID}/feed?fields=message,full_picture&access_token=${pageAccessToken}`
+			axios.get(feedURL).then(({ data: posts}) => {
+				setFacebookPosts(posts.data)
+			})
+		}
+	}, [pageAccessToken])
 
 	return <>
 		<Helmet>
@@ -13,16 +33,19 @@ const BlogView = () => {
 		</Helmet>
 		<Container className="text-center my-5 pb-5">
 			<h1 className="custom-font my-5">Останні новини</h1>
-			<FacebookProvider appId="2185912735037143">
-				<Page href="https://www.facebook.com/myz.shpytky" tabs="timeline"
-					width={windowWidth} // max 500px
-					height="700" // fullscreen desktop height
-					small-header="true"
-					adapt-container-width="true"
-					hide-cover="true"
-					show-facepile="false"
-				/>
-			</FacebookProvider>
+			<article>
+				{facebookPosts.length
+					? <div className={styles.postsContainer}>
+							{facebookPosts.map((item) => {
+								return <div key={item.id}>
+									<p>{item.message}</p>
+									<img src={item.full_picture} />
+								</div>
+							})}
+						</div>
+					: <div>loading...</div>
+				}
+			</article>
 		</Container>
 	</>
 }
