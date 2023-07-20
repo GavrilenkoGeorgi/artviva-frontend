3a3855b73ad2fcb85895f7396a4d382a0ac0b159
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-07-20 17:57:30 +0300

    Update 'news' page
    - 'Load more' button to load more posts on demand
    - Animated post layout a bit, posts now fade in on scroll

--- src/components/views/public/BlogView.jsx
+++ src/components/views/public/BlogView.jsx
@@ -5,32 +5,42 @@ import { Helmet } from 'react-helmet'
 import { Container } from 'react-bootstrap'
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 import { SimpleSpinner } from '../../common/spinners'
+import ScrollAnimation from 'react-animate-on-scroll'
 
 import styles from './BlogView.module.sass'
 
 const BlogView = () => {
 
-	const [ pageAccessToken, setPageAccessToken ] = useState(null)
+	const [ fetching, setFetching ] = useState(null)
 	const [ facebookPosts, setFacebookPosts ] = useState([])
+	const [ postsURL, setPostsURL ] = useState(null)
+	const [ paging, setPaging ] = useState(null)
 
 	// ToDo: move this somewhere
+	// --- Get page access token ---
 	useEffect(() => {
 		const pageAccess = `https://graph.facebook.com/${process.env.REACT_APP_FACEBOOK_PAGE_ID}?fields=access_token&access_token=${process.env.REACT_APP_FACEBOOK_ACCESS_TOKEN}`
 		axios.get(pageAccess).then((response) => {
-			setPageAccessToken(response.data.access_token)
+			setPostsURL(`https://graph.facebook.com/v17.0/${process.env.REACT_APP_FACEBOOK_PAGE_ID}/feed?fields=message,full_picture,created_time,permalink_url&access_token=${response.data.access_token}`)
 		})
 	}, [])
 
 	// and this
 	useEffect(() => {
-		if (pageAccessToken) {
-			const feedURL = `https://graph.facebook.com/v17.0/${process.env.REACT_APP_FACEBOOK_PAGE_ID}/feed?fields=message,full_picture,created_time,permalink_url&access_token=${pageAccessToken}`
-			axios.get(feedURL).then(({ data: posts}) => {
+		setFetching(true)
+		if (postsURL) {
+			axios.get(postsURL).then(({ data: posts }) => {
+				setPaging(posts.paging)
 				const filtered = posts.data.filter(post => post.hasOwnProperty('message'))
-				setFacebookPosts(filtered)
+				setFacebookPosts(prevState => [ ...prevState, ...filtered ])
+				setFetching(false)
 			})
 		}
-	}, [pageAccessToken])
+	}, [postsURL])
+
+	const loadMore = () => {
+		setPostsURL(paging.next)
+	}
 
 	const formatDate = date => {
 		const result = moment(date).format('LL')
@@ -46,28 +56,37 @@ const BlogView = () => {
 			<h1 className="custom-font my-5">Останні новини</h1>
 			{facebookPosts.length
 				? <div className={styles.postsContainer}>
-					<ResponsiveMasonry columnsCountBreakPoints={{350: 1, 1440: 2}}>
-						<Masonry gutter='2.5rem'>
-							{facebookPosts.map((item) => {
-								return <a href={item.permalink_url} key={item.id}>
-									<div className={styles.post}>
-										<div className={styles.imgContainer}>
-											<img
-												className={styles.postImg}
-												src={item.full_picture}
-											/>
-										</div>
-										<p className={styles.message}>
-											{item.message}
-										</p>
-										<p className={styles.dateStamp}>
-											{formatDate(item.created_time)}
-										</p>
-									</div>
-								</a>
-							})}
-						</Masonry>
-					</ResponsiveMasonry>
+						<ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 1440: 2, 1920: 3 }}>
+							<Masonry gutter='2.5rem'>
+								{facebookPosts.map((item) => {
+									return <ScrollAnimation animateIn="fadeIn" key={item.id} >
+										<a href={item.permalink_url}>
+											<div className={styles.post}>
+												<div className={styles.imgContainer}>
+													<img
+														className={styles.postImg}
+														src={item.full_picture}
+													/>
+												</div>
+												<p className={styles.message}>
+													{item.message}
+												</p>
+												<p className={styles.dateStamp}>
+													{formatDate(item.created_time)}
+												</p>
+											</div>
+										</a>
+									</ScrollAnimation>
+								})}
+							</Masonry>
+						</ResponsiveMasonry>
+						<div className={styles.paginationContainer}>
+							<button
+								onClick={() => loadMore()}
+							>
+								{ fetching ? <SimpleSpinner /> : "Завантажити ще" }
+							</button>
+						</div>
 					</div>
 				: <SimpleSpinner />
 			}
