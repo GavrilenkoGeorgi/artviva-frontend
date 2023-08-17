1cf8cbc833bfa11c776e1f352652ca77ff6e4b97
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-08-17 15:24:52 +0300

    Add new data fileds on fetch

--- src/services/facebookAPI.js
+++ src/services/facebookAPI.js
@@ -8,7 +8,7 @@ const getAccessToken = async () => {
 
 const getPostsURL = ( access_token ) => {
 	// eslint-disable-next-line
-	return `https://graph.facebook.com/v17.0/${process.env.REACT_APP_FACEBOOK_PAGE_ID}/feed?fields=message,full_picture,created_time,permalink_url&access_token=${access_token}`
+	return `https://graph.facebook.com/v17.0/${process.env.REACT_APP_FACEBOOK_PAGE_ID}/feed?fields=message,full_picture,created_time,permalink_url,shares&access_token=${access_token}`
 }
 
 export { getAccessToken, getPostsURL }
