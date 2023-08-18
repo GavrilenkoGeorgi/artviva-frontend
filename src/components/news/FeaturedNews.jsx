08655007c87260d278a5b1c0368d208c497b7dd5
Georgi Gavrilenko <gavrilenko.georgi@gmail.com>
2023-08-18 12:34:55 +0300

    Add 'fadeInUp' animation

--- src/components/news/FeaturedNews.jsx
+++ src/components/news/FeaturedNews.jsx
@@ -4,8 +4,8 @@ import axios from 'axios'
 import { getAccessToken, getPostsURL } from '../../services/facebookAPI'
 import { formatDate } from '../../utils/datesAndTime'
 
+import ScrollAnimation from 'react-animate-on-scroll'
 import styles from './FeaturedNews.module.sass'
-
 
 const FeaturedNews = () => {
 
@@ -38,31 +38,39 @@ const FeaturedNews = () => {
 		</div>
 		<div className={styles.newsItems}>
 			{posts && <>
-				{posts.map((item) => (
-					<div className={styles.postData} key={item.id}>
-						<div className={styles.dateStamp}>
-							{formatDate(item.created_time)}
-						</div>
-						<a href={item.permalink_url}
-							className={styles.cardLink}
-						>
-							<div className={styles.textLayout}>
-								<p>
-									{item.message}
-								</p>
+				{posts.map(item => (
+					<ScrollAnimation
+						animatePreScroll={false}
+						animateOnce={true}
+						animateIn="fadeInUp"
+						delay={250}
+						key={item.id}
+					>
+						<div className={styles.postData} >
+							<div className={styles.dateStamp}>
+								{formatDate(item.created_time)}
 							</div>
-						</a>
-						<div className={styles.hashtags}>
-							{item.hashtags.map((tag) => (
-								<a
-									className={styles.hashtag}
-									href={`https://www.facebook.com/hashtag/${tag.slice(1)}`}
-									key={tag}>
-									{tag}{' '}
-								</a>
-							))}
+							<a href={item.permalink_url}
+								className={styles.cardLink}
+							>
+								<div className={styles.textLayout}>
+									<p>
+										{item.message}
+									</p>
+								</div>
+							</a>
+							<div className={styles.hashtags}>
+								{item.hashtags.map(tag => (
+									<a
+										className={styles.hashtag}
+										href={`https://www.facebook.com/hashtag/${tag.slice(1)}`}
+										key={tag}>
+										{tag}{' '}
+									</a>
+								))}
+							</div>
 						</div>
-					</div>
+					</ScrollAnimation>
 				))}
 			</>}
 		</div>
